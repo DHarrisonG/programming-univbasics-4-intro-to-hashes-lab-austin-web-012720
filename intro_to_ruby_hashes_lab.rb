850727975d54890813e2ab9e0ccc36bd32fc63d2
Doug Harrison <dougharrisong@gmail.com>
2020-01-09 22:34:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,9 +27,4 @@ def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
 end
 
-def update_counting_hash(hash, key)
-  if hash[key]
-    hash[key] += 1
-  else
-    hash[key] = 1
-end
+
